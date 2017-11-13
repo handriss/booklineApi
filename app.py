66dd71d798280813dcbfbe2a21ea8b13e6aea179
handriss <andrashinkel@gmail.com>
2017-11-13 19:37:26 +0100

    spike post request

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/", methods=['POST'])
+def json():
+
+    if request.json:
+        book_data = request.json
+        return "Author: " + book_data.get("author") + " Title: " + book_data.get("title")
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
     app.run()
